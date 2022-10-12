6ef1cd002e216a6b63ba2cdaf8a61e69b1bd8c1a
abhi2666 <95623413+abhi2666@users.noreply.github.com>
2022-10-12 16:55:27 +0530

    bisection method

---
+++ bisectionMethod.c
@@ -0,0 +1,60 @@
+// Bisection method -- most slowest method to find the 
+// root of a given equation..
+
+#include<stdio.h>
+#define e 0.001
+
+float func(float x)
+{
+    // function of which we want to find the root
+    return x*x*x*x - x - 10;
+}
+
+void bisection(float a, float b)
+{
+    float c = 0.0;
+    int itr = 0;
+    while((b - a) >= e)
+    {
+        // finding the mid point of the guessed values
+        c = (a + b)/2.0;
+
+        // check if c is the root or not
+        if(func(c) == 0.0001)
+        {
+            // means you have got the root
+            printf("The root is --> %f\n", c);
+            printf("Total no. of iterations are --> %d", itr);
+            return;
+        }
+        else if(func(a)*func(c) < 0)
+        {
+            b = c;
+        }
+        else
+        {
+            a = c;
+        }
+        itr++;
+    }
+    printf("The required root is --> %f\n", c);
+    printf("Total no. of iterations are --> %d\n", itr);
+
+}
+
+int main()
+{
+    float a, b;
+    do
+    {
+        printf("Enter first two guesses: ");
+        scanf("%f%f", &a, &b);
+    } while (func(a)*func(b) >= 0);
+    printf("Your correct guesses are: %0.2f %0.2f\n", a, b);
+    // your result i.e., root will lie between a and b
+    bisection(a, b);
+    
+
+    
+    return 0;
+}
